5114fa911ccd38f87b77998c91ecf5631c2613c4
aaronsfunstuff <135081747+aaronsfunstuff@users.noreply.github.com>
2024-07-09 11:48:22 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// scripts.js
+document.getElementById('email').addEventListener('click', function() {
+    alert('Email: DontWannaShare@example.com');
+});
+
+document.getElementById('phone').addEventListener('click', function() {
+    alert('Phone: Don\'t wanna share');
+});
+
+document.getElementById('discord').addEventListener('click', function() {
+    alert('Discord: orange_ramune');
+});
